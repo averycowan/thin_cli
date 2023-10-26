35e1fc9eb4c55b081c361a357f87a0eaccbb682f
Avery Cowan <cowana@google.com>
2023-10-26 15:17:03 -0700

    Gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thin_cli',
+    version='1.0.0',
+    description='A thin command line interface framework.',
+    author='Avery Cowan',
+    url='https://github.com/averycowan/lite_cli',
+    packages=find_packages(),
+    install_requires=[],
+)
